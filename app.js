00dabaa464f41e446f134b93cea2cb6b2849a198
adarsht0001 <adarsht00001@gmail.com>
2023-10-23 23:31:57 +0530

    add: comments

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ limit: "50mb" }));
 
 app.post("/upload", upload.single("pdf"), async (req, res) => {
     console.log("here");
-    // console.log(req.body.pages); //array of pages needed
-    const pages = [1, 2, 3, 4].map((e) => parseInt(e));
+    // console.log(req.body.pages); //array of pages needed for croping
+    // const pages = [1, 2, 3, 4].map((e) => parseInt(e)); //if array is string annel use cheytho
     const pdfFile = await PDFDocument.load(req.file.buffer);
     const pdfDoc = await PDFDocument.create();
     const copiedPages = await pdfDoc.copyPages(pdfFile, pages);
