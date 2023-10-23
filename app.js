4bfa76d9dafb4e4974d9d78563ee9f04864a825d
adarsht0001 <adarsht00001@gmail.com>
2023-10-24 01:57:55 +0530

    add: image cropping

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ limit: "50mb" }));
 
 app.post("/upload", upload.single("pdf"), async (req, res) => {
     console.log("here");
-    // console.log(req.body.pages); //array of pages needed for croping
-    // const pages = req.body.pages.map((e) => parseInt(e)); //if array is string annel use cheytho
+    console.log(req.body.pages); //array of pages needed for croping
+    const pages = req.body.pages.map((e) => parseInt(e)); //if array is string annel use cheytho
     const pdfFile = await PDFDocument.load(req.file.buffer);
     const pdfDoc = await PDFDocument.create();
     const copiedPages = await pdfDoc.copyPages(pdfFile, pages);
